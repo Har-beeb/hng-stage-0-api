00f2fd337c9ab40c740ea1481aab45bf1d7b978d
Har-beeb <harbeeb.dev@gmail.com>
2026-04-17 12:42:09 +0100

    fix vercel crash and removed dubug comments

--- index.js
+++ index.js
@@ -16,31 +16,3 @@ mongoose
   .catch((error) => {
     logger.error("Error connecting to MongoDB:", error.message);
   });
-
-// // Fallback to process.env directly just in case config.js fails on Vercel
-// const MONGODB_URI = config.MONGODB_URI || process.env.MONGODB_URI;
-
-// if (!MONGODB_URI) {
-//   logger.error("FATAL ERROR: MONGODB_URI is not defined in the environment!");
-// } else {
-//   logger.info("connecting to MongoDB...");
-//   mongoose
-//     .connect(MONGODB_URI)
-//     .then(() => {
-//       logger.info("Connected to MongoDB");
-//     })
-//     .catch((error) => {
-//       logger.error("Error connecting to MongoDB:", error.message);
-//     });
-// }
-
-// // Local fallback: Only run app.listen if we are NOT on Vercel
-// if (process.env.NODE_ENV !== "production") {
-//   const PORT = process.env.PORT || 3000;
-//   app.listen(PORT, () => {
-//     logger.info(`Server running on port ${PORT}`);
-//   });
-// }
-
-// // CRITICAL FOR VERCEL: Export the app directly
-// module.exports = app;
